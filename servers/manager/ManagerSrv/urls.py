fac60883b05ff4e67b96b6518949e7680f17f295
Panagiotis Karkazis <pkarkazis@synelixis.com>
2016-03-08 09:23:50 +0200

    monServ v1

--- servers/manager/ManagerSrv/urls.py
+++ servers/manager/ManagerSrv/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rest_framework import routers
-from quickstart import views
+from usersMng import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
