52d6795f6e43649c034d26e3ee839b2c8b03ffba
Panagiotis Karkazis <pkarkazis@synelixis.com>
2017-03-28 17:15:38 +0300

    websockets

--- manager/run.sh
+++ manager/run.sh
@@ -15,6 +15,5 @@ fi && \
 cp /opt/Monitoring/apache-site /etc/apache2/sites-available/000-default.conf && \
 sed -i.bak 's/.*Listen.*/Listen '8000'/' /etc/apache2/ports.conf && \
 chown -R www-data:www-data /opt/Monitoring && \
-service apache2 restart 
-python /opt/Monitoring/tornadoWS/son_mon_ws.py &
+service apache2 restart
 tail -f /dev/null
--- prometheus/run.sh
+++ prometheus/run.sh
@@ -3,7 +3,6 @@ a2enmod wsgi && \
 a2ensite PromCnf && \
 sed -i.bak 's/.*Listen.*/Listen '9089'/' /etc/apache2/ports.conf && \
 service apache2 restart 
+python /opt/Monitoring/tornadoWS/son_mon_ws.py &
 python /opt/Monitoring/prometheus/alertMng/alertmanager.py &
 /opt/Monitoring/prometheus/prometheus -config.file=/opt/Monitoring/prometheus/prometheus.yml -storage.local.retention 680h0m0s  -storage.remote.influxdb-url http://influx:8086 -storage.remote.influxdb.database "prometheus" -storage.remote.influxdb.retention-policy "default" >/dev/null 2>&1
-
-
