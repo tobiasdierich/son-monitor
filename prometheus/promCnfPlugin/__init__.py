91f5e9394f42d5bd454c42618ff2f7a28ea98277
Tobias Dierich <git@tobiasdierich.de>
2017-12-04 12:52:16 +0100

    Add endpoints to add/remove prometheus scrape jobs

--- prometheus/promCnfPlugin/__init__.py
+++ prometheus/promCnfPlugin/__init__.py
@@ -27,27 +27,28 @@ partner consortium (www.sonata-nfv.eu).
 '''
 
 import logging, logging.handlers, sys, yaml, os.path, httplib
-from flask import json,request,Flask, url_for,jsonify
+from flask import json, request, Flask, url_for, jsonify
 from functools import wraps
 from ruleFile import fileBuilder
 
-
 app = Flask(__name__)
 
-global promPath 
+global promPath
 promPath = '/opt/Monitoring/prometheus/'
 
 
 @app.route("/")
 def hello():
-    urls =  "I am alive..."
+    urls = "I am alive..."
     return urls
 
 
 '''
 {"service":"NF777777","rules":[{"description": "Rule_#1", "summary": "Rule combines two metrics", "duration": "4m", "notification_type": 2, "condition": "metric1 - metric2 > 0.25", "name": "Rule 1", "labels":["id = docker","mode = user"]},{"description": "Rule_#2", "summary": "Rule combines two other metrics", "duration": "4m", "notification_type": 2, "condition": "metric3 - metric4 > 0.25", "name": "Rule 2", "labels":["id = docker","mode = user1"]}]}
 '''
-@app.route('/prometheus/rules', methods = ['POST'])
+
+
+@app.route('/prometheus/rules', methods=['POST'])
 def api_rules():
     if request.method == 'POST':
         conf = json.loads(request.data)
@@ -64,47 +65,48 @@ def api_rules():
         rf = fileBuilder(srv_id, conf['rules'], promPath)
         status = rf.writeFile();
         message = {
-                'status': 200,
-                'message': status,
-            } 
+            'status': 200,
+            'message': status,
+        }
         resp = jsonify(message)
         resp.status_code = 200
         return resp
-    elif request.method == 'GET':    
+    elif request.method == 'GET':
         return '(GET) get alert for '
-    elif request.method == 'PUT':    
+    elif request.method == 'PUT':
         return '(PUT) get alert for '
 
-@app.route('/prometheus/rules/<srv_id>', methods = ['GET','DELETE'])
+
+@app.route('/prometheus/rules/<srv_id>', methods=['GET', 'DELETE'])
 def api_rules_per_srv(srv_id):
     if request.method == 'DELETE':
-        fname = promPath+'rules/'+srv_id.strip()+'.rules'
+        fname = promPath + 'rules/' + srv_id.strip() + '.rules'
         if os.path.isfile(fname):
             os.remove(fname)
-            with open(promPath+'prometheus.yml', 'r') as conf_file:
+            with open(promPath + 'prometheus.yml', 'r') as conf_file:
                 conf = yaml.load(conf_file)
                 for rf in conf['rule_files']:
                     if fname in rf:
                         conf['rule_files'].remove(rf)
-                        with open(promPath+'prometheus.yml', 'w') as yml:
+                        with open(promPath + 'prometheus.yml', 'w') as yml:
                             yaml.safe_dump(conf, yml)
                         continue
-	        reloadServer()
+                reloadServer()
             message = {
                 'status': 200,
-                'message': 'File DELETED (' +fname+')',
+                'message': 'File DELETED (' + fname + ')',
             }
         else:
             message = {
                 'status': 200,
-                'message': 'File NOT FOUND (' +fname+')',
+                'message': 'File NOT FOUND (' + fname + ')',
             }
         resp = jsonify(message)
-        resp.status_code = 404  
+        resp.status_code = 404
         return resp
 
     elif request.method == 'GET':
-        fname = promPath+'rules/'+srv_id.strip()+'.rules'
+        fname = promPath + 'rules/' + srv_id.strip() + '.rules'
         if os.path.isfile(fname):
             with open(fname, 'r') as conf_file:
                 conf = yaml.load(conf_file)
@@ -113,40 +115,51 @@ def api_rules_per_srv(srv_id):
         else:
             message = {
                 'status': 200,
-                'message': 'File ' +fname+' not found',
+                'message': 'File ' + fname + ' not found',
             }
             return jsonify(message)
-    
-@app.route('/prometheus/configuration', methods = ['GET', 'POST'])
+
+
+@app.route('/prometheus/configuration', methods=['GET', 'POST'])
 def api_conf():
     if request.method == 'GET':
-        with open(promPath+'prometheus.yml', 'r') as conf_file:
+        with open(promPath + 'prometheus.yml', 'r') as conf_file:
             conf = yaml.load(conf_file)
             js_obj = json.dumps(conf)
-            #print(js_obj)
+            # print(js_obj)
         return js_obj
-    elif request.method == 'POST':    
+    elif request.method == 'POST':
         conf = json.loads(request.data)
         rf = fileBuilder('prometheus.yml', conf, promPath)
-        resp=rf.buildConf()
+        resp = rf.buildConf()
         message = {
-                'status': 200,
-                'message': resp,
-            }
+            'status': 200,
+            'message': resp,
+        }
         return jsonify(message)
-    
+
+
+@app.route('/prometheus/configuration/jobs', methods=['POST'])
+def add_job():
+    return "Add job"
+
+
+@app.route('/prometheus/configuration/jobs', methods=['DELETE'])
+def remove_job():
+    return "Remove job"
 
 
 @app.errorhandler(404)
 def not_found(error=None):
     message = {
-            'status': 404,
-            'message': 'Not Found: ' + request.url,
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
     }
     resp = jsonify(message)
     resp.status_code = 404
 
     return resp
+
 
 def reloadServer():
     httpServ = httplib.HTTPConnection("localhost", 9090)
@@ -161,10 +174,8 @@ def reloadServer():
         return 'FAILED'
 
 
-
-
 if __name__ == "__main__":
-    global promPath 
+    global promPath
     promPath = '/opt/Monitoring/prometheus/'
     global logger
     logger = logging.getLogger('MyLogger')
